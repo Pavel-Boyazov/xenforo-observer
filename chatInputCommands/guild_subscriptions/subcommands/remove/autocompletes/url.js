c6a20c4bead7fb794fb4a23dd6407396fcfb5731
Pavel-Boyazov <65123753+Pavel-Boyazov@users.noreply.github.com>
2026-01-20 02:45:26 +0300

    create 'guild_subscriptions remove' command

---
+++ chatInputCommands/guild_subscriptions/subcommands/remove/autocompletes/url.js
@@ -0,0 +1,50 @@
+const Autocomplete = require("../../../../../classes/Autocomplete");
+const { getLinks } = require("../../../../../mfunc");
+const { $Enums } = require("../../../../../prisma/generated");
+const { apiInstance } = require("../../../../../utils");
+
+module.exports = new Autocomplete({
+	optionName: "ссылка",
+	getRespond(interaction) {
+		const channelId = interaction.options.getString("канал") ?? undefined;
+
+		return getLinks({ subscriptions: { some: { guildId: interaction.guildId, targetId: channelId } } }).then((links) =>
+			Promise.all(
+				links.map(async ({ type, id }) => {
+					let path;
+
+					switch (type) {
+						case $Enums.LinkType.FORUM:
+							path = `/forums/${id}`;
+							break;
+						case $Enums.LinkType.THREAD:
+							path = `/threads/${id}`;
+							break;
+					}
+
+					let title;
+
+					try {
+						await apiInstance.get(path).then(({ data }) => {
+							switch (type) {
+								case $Enums.LinkType.FORUM:
+									title = data.forum.title;
+									break;
+								case $Enums.LinkType.THREAD:
+									title = data.thread.title;
+									break;
+							}
+						});
+					} catch (error) {
+						if (error.status !== 403 && error.status !== 404) console.error(error);
+					}
+
+					return {
+						name: `https://${process.env.FORUM_HOSTNAME}${path}${title ? ` (${title})` : ""}`,
+						value: `https://${process.env.FORUM_HOSTNAME}${path}`,
+					};
+				}),
+			),
+		);
+	},
+});
---
+++ chatInputCommands/guild_subscriptions/subcommands/remove/index.js
@@ -0,0 +1,152 @@
+/**
+ * @import { LinkType } from "../../../../../../prisma/generated"
+ */
+
+const { PrismaClientKnownRequestError } = require("@prisma/client/runtime/client");
+const {
+	ApplicationCommandOptionType,
+	MessageFlags,
+	bold,
+	hyperlink,
+	Locale,
+	ChannelType,
+	channelMention,
+} = require("discord.js");
+
+const Subcommand = require("../../../../classes/Subcommand");
+const { subscriptions } = require("../../../../mfunc");
+const { $Enums } = require("../../../../prisma/generated");
+const { urlRegex, apiInstance } = require("../../../../utils");
+
+module.exports = new Subcommand({
+	discordData: {
+		name: "удалить",
+		nameLocalizations: {
+			[Locale.EnglishUS]: "remove",
+			[Locale.EnglishGB]: "remove",
+		},
+		description: "Отписаться от уведомлений",
+		options: [
+			{
+				type: ApplicationCommandOptionType.String,
+				name: "ссылка",
+				nameLocalizations: {
+					[Locale.EnglishUS]: "url",
+					[Locale.EnglishGB]: "url",
+				},
+				description: "Ссылка на отслеживаемый форум/тему/сообщение",
+				autocomplete: true,
+				required: true,
+				minLength: 31,
+			},
+			{
+				type: ApplicationCommandOptionType.Channel,
+				name: "канал",
+				nameLocalizations: {
+					[Locale.EnglishUS]: "channel",
+					[Locale.EnglishGB]: "channel",
+				},
+				description: "Канал, в который отправляются уведомления (текущий если не указано)",
+				channelTypes: [
+					ChannelType.GuildText,
+					ChannelType.GuildVoice,
+					ChannelType.PublicThread,
+					ChannelType.PrivateThread,
+					ChannelType.GuildStageVoice,
+					ChannelType.GuildAnnouncement,
+					ChannelType.AnnouncementThread,
+				],
+			},
+		],
+	},
+	execute(interaction) {
+		let url;
+		try {
+			url = new URL(interaction.options.getString("ссылка", true));
+		} catch (error) {
+			if (error.message === "Invalid URL")
+				return interaction.reply({
+					content: "Пришлите ссылку верного формата",
+					flags: MessageFlags.Ephemeral,
+				});
+
+			console.error(error);
+
+			return interaction.reply({ content: "При чтении ссылки произошла ошибка", flags: MessageFlags.Ephemeral });
+		}
+
+		if (url.host !== process.env.FORUM_HOSTNAME)
+			return interaction.reply({
+				content: `Бот поддерживает только https://${process.env.FORUM_HOSTNAME}/`,
+				flags: MessageFlags.Ephemeral,
+			});
+
+		const { type: urlType, id } = url.pathname.match(urlRegex).groups;
+		/** @type {LinkType} */
+		let linkType;
+
+		if (urlType === "forums") linkType = $Enums.LinkType.FORUM;
+		else linkType = $Enums.LinkType.THREAD;
+
+		const channelId = interaction.options.getChannel("канал")?.id ?? interaction.channelId;
+
+		return subscriptions
+			.delete({
+				targetId_linkType_linkId: {
+					targetId: channelId,
+					linkType: linkType,
+					linkId: +id,
+				},
+			})
+			.then(async () => {
+				let typeString;
+
+				switch (linkType) {
+					case $Enums.LinkType.FORUM:
+						typeString = "форума";
+						break;
+					case $Enums.LinkType.THREAD:
+						typeString = "темы";
+						break;
+				}
+
+				let title;
+
+				try {
+					if (linkType === $Enums.LinkType.FORUM)
+						await apiInstance.get(`/forums/${id}`).then(({ data }) => {
+							title = data.forum.title;
+						});
+					else
+						await apiInstance.get(`/threads/${id}`).then(({ data }) => {
+							title = data.thread.title;
+						});
+				} catch (error) {
+					if (error.status !== 403 && error.status !== 404) console.error(error);
+				}
+
+				return interaction.reply({
+					content:
+						`Вы отписали канал ${channelMention(channelId)} от обновлений ${typeString} ` +
+						bold(title ? hyperlink(title, url.toString()) : url.toString()),
+					flags: MessageFlags.Ephemeral,
+				});
+			})
+			.catch((err) => {
+				if (err instanceof PrismaClientKnownRequestError && err.code === "P2025")
+					return interaction.reply({
+						content:
+							`Данная подписка в канале ${channelMention(channelId)} ` +
+							"уже отсутствует, вам не будут присылаться обновления",
+						flags: MessageFlags.Ephemeral,
+					});
+
+				console.error(err);
+
+				return interaction.reply({
+					content: "К сожалению, не удалось отписать канал от обновлений. Повторите попытку позже",
+					flags: MessageFlags.Ephemeral,
+				});
+			});
+	},
+});
